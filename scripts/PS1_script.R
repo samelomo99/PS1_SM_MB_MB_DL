f3caefe6eaadef489a8e87ad18d22e8bf843880e
samelomo99 <s.melom2@uniandes.edu.co>
2025-01-23 18:20:00 -0500

    Create PS1_script.R

---
+++ scripts/PS1_script.R
@@ -0,0 +1,6 @@
+#----------------------------------------------------#
+## ------- Problem Set 1: Predicting Income ------- ##
+## - Santiago Melo - Miguel Blanco - María Bernal - ##
+#----------------------------------------------------#
+
+## Este es el R de prueba, para saber como se puede actualizar desde GitHub.
